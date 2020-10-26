daa18a3fe9b6ac74a45ec8dd97fe2b79127cabe2
GuyLeifer <67858228+GuyLeifer@users.noreply.github.com>
2020-10-26 20:25:25 +0200

    add two charts and select bar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Select from 'react-select';
+import makeAnimated from 'react-select/animated';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Brush, Tooltip , Legend, AreaChart, Area} from 'recharts';
 import './App.css';
 
+const animatedComponents = makeAnimated();
+
+function getRandomColor() {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 function App() {
+
+  const [data, setData] = useState();
+  const [options, setOptions] = useState();
+  const [selected, setSelected] = useState([]);
+
+    useEffect( () => {
+      axios.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+      .then(({ data:csv }) => {
+        const countries = csv.split('\n').map(dataBlock => {
+          const regex = /".*,.*"/
+          if (dataBlock.search(regex) !== -1){
+            const problemWord = dataBlock.match(regex)[0]
+            const fixedWord = problemWord.replace(',', '-')
+            const fixedData = dataBlock.replace(regex, fixedWord)
+            return fixedData.split(',')
+          } else {
+            return dataBlock.split(',')
+          }
+        })
+        countries.map(country => (
+          country.color = getRandomColor()
+        ))
+        console.log('countries: ', countries);
+        
+        setOptions(countries.map((country) => {
+          if (country[0] !== "") {
+            return ({value: country[1] + " (" + country[0] +")", label: country[1] + " (" + country[0] +")", color: country.color})
+          } else {
+            return ({value: country[1], label: country[1], color: country.color})
+          }
+        }))
+
+    const titles = countries.shift(0);
+    console.log("titles:" , titles)
+    const dates = titles.slice(4).map((date, i) => {
+      const dateObject = {date: date}
+      countries.forEach(country => {
+        if (country[0] !== "") {
+          dateObject[country[1] + " (" + country[0] +")"] = country[i + 4]
+        } else {
+          dateObject[country[1]] = country[i + 4];
+        }
+      })
+      return dateObject;
+    })
+    setData(dates);
+    console.log("data: ", dates)
+  })
+}, [selected]);
+
+  const selectionChange = (choosens) => {
+    setSelected(choosens);
+  }
+
+console.log("options:" , options)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Corona Chart:</h1>
+      <h2>See What's Up Around The World</h2> 
+      <Select 
+      className="select"
+      options={options}
+      isMulti 
+      placeholder="Select Countries..."
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      onChange={selectionChange}
+      theme={theme => ({
+        ...theme,
+        borderRadius: 0,
+        colors: {
+          ...theme.colors,
+          primary25: '#1e90ff',
+        },
+      })}
+      />
+
+      <LineChart 
+      className="chart"
+      width={700} 
+      height={700} 
+      data={data}
+      >
+        {selected && (
+          selected.map((country)=> (
+            <Line type="monotone" dataKey={country.value} stroke={country.color} />
+          ))
+        )}
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Brush />
+        <Tooltip  />
+        <Legend />
+      </LineChart>
+
+      <AreaChart 
+      className="chart"
+      width={700} 
+      height={700}  
+      data={data}
+      >
+        <CartesianGrid strokeDasharray="3 3"/>
+        <XAxis dataKey="date"/>
+        <YAxis/>
+        <Tooltip/>
+        <Legend />
+        <Brush />
+        {selected && (
+          selected.map((country)=> (
+            <Area type="monotone" dataKey={country.value} stackId="1" stroke={country.color} fill={country.color} />
+          ))
+        )}
+      </AreaChart>
     </div>
   );
 }
